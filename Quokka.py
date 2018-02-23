26c7b3d023b80930fe98d6e59e8f4bd08e34f30c
svvarik <sv.varikooty@mail.utoronto.ca>
2018-02-22 21:12:48 -0500

    Updated python function to include sign_in_page

--- Quokka.py
+++ Quokka.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return render_template("user_info.html")
+@app.route('/', methods=['GET', "POST"])
+def sign_in_page():
+    if request.method == "GET":
+        return render_template("user_info.html")
+    if request.method == "POST":
+        # First check if this user exists (by email)
+        # If user doesn't exist, create new user.
+        #
+        return 'YOU"RE ALL SIGNED UP '
 
 
 if __name__ == '__main__':
