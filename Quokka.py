cf97b549397bb344745f783ff9a67e5d4c54be30
svvarik <sv.varikooty@mail.utoronto.ca>
2018-04-03 19:28:53 -0400

    Added new sign in page, and in doing so, created new CSS documents. > > Details: There is bad naming, and will currently require a naming overhaul, along with a document to formalize naming and keep it consistent

--- Quokka.py
+++ Quokka.py
@@ -1,17 +1,44 @@
 from flask import Flask, render_template, request
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from pymongo import MongoClient
+
+
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', "POST"])
-def sign_in_page():
+# file = open("MONGODB_URI.txt", 'r')
+# MONGODB_URI = file.readline()
+# file.close()
+#
+# client = MongoClient(MONGODB_URI)
+# db = client.get_database('platters_hacks')
+
+
+@app.route('/', methods=['GET'])
+def landing_page():
     if request.method == "GET":
-        return render_template("user_info.html")
+        return render_template("landing_page.html")
+    else:
+        return "Error, you've somehow landed on a broken page"
+
+
+@app.route('/user_registration', methods=['GET', "POST"])
+def sign_up():
+    if request.method == "GET":
+        return render_template("sign_up_page.html")
     if request.method == "POST":
+        return "HI"
         # First check if this user exists (by email)
         # If user doesn't exist, create new user.
-        #
-        return 'YOU"RE ALL SIGNED UP '
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "GET":
+        return render_template("login_page.html")
+    if request.method == "POST":
+        return "YOU'VE LOGGED IN"
 
 
 if __name__ == '__main__':
